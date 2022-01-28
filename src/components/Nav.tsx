a9b54c50f7ce66c2e1565deb8f2b7e9905423be8
GyoHeonLee <ghl12078@gmail.com>
2022-01-28 21:00:00 +0900

    feat: navlink make

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,47 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 function Nav() {
   return (
     <Navbar>
-      <Link to="/News">News</Link>
-      <Link to="/Show">Show</Link>
-      <Link to="/Ask">Ask</Link>
-      <Link to="/Jobs">Jobs</Link>
+      <NavLink
+        className={({ isActive }) => (isActive ? "selected" : "")}
+        to="/News"
+      >
+        News
+        <BottomLine />
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "selected" : "")}
+        to="/Show"
+      >
+        Show
+        <BottomLine />
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "selected" : "")}
+        to="/Ask"
+      >
+        Ask
+        <BottomLine />
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "selected" : "")}
+        to="/Jobs"
+      >
+        Jobs
+        <BottomLine />
+      </NavLink>
     </Navbar>
   );
 }
 
+const BottomLine = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-top: 4px;
+  background-color: #f0f0f6;
+`;
 const Navbar = styled.nav`
   width: 335px;
   height: 40px;
@@ -23,8 +53,16 @@ const Navbar = styled.nav`
     display: inline-block;
     width: 83.75px;
     height: 24px;
-    border-bottom: 1px solid #f0f0f6;
     margin: 12px 0 4px 0;
+    &.selected {
+      color: ${(props) => props.theme.accentColor};
+      font-weight: 500;
+    }
+    &.selected div {
+      height: 2px;
+      background-color: ${(props) => props.theme.accentColor};
+      border-radius: 2px 2px 0 0;
+    }
   }
 `;
 
