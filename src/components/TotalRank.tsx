634d24eceb2eec6f4fec766b255d13306ea85c06
GyoHeonLee <ghl12078@gmail.com>
2022-02-01 00:01:37 +0900

    style: roman fonts add

--- src/components/TotalRank.tsx
+++ src/components/TotalRank.tsx
@@ -5,7 +5,7 @@ function TotalRank() {
   return (
     <Total>
       <h1>
-        Total <br /> Ranking 5 Now
+        Current <br /> Total Top 5
       </h1>
       <section>
         <TotalRankCard />
@@ -16,13 +16,18 @@ function TotalRank() {
 
 const Total = styled.section`
   width: 100%;
-  height: 539px;
   h1 {
+    @font-face {
+      font-family: "Times New Roman", Times, serif;
+      src: url("/public/times.ttf");
+    }
+    font-family: "Times New Roman";
+    font-weight: 400;
     display: inline-block;
     width: 335px;
-    height: 56px;
-    font-size: 28px;
-    margin: 32px 20px 24px;
+    font-size: 32px;
+    line-height: 40px;
+    margin: 36px 20px 24px;
   }
   section {
     height: 427px;
