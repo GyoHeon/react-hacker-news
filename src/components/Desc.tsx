c8355abbeda692ed0288cb0a197c1d77c259934b
GyoHeonLee <ghl12078@gmail.com>
2022-02-06 02:06:40 +0900

    feat: data load

--- src/components/Desc.tsx
+++ src/components/Desc.tsx
@@ -60,12 +60,38 @@ function Desc() {
             </svg>
 
             <span>{data?.by}</span>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6 5L9 8L6 11"
+                stroke="#767676"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
           </div>
           <a
             href={`https://news.ycombinator.com/item?id=${data?.id}`}
             target="_blank"
           >
             news.ycombinator.com
+            <svg
+              width="10"
+              height="5"
+              viewBox="0 0 10 5"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1 2.5C1 1.645 1.645 1 2.5 1H3.5C3.5 1 4 1 4 0.5C4 0 3.5 0 3.5 0H2.5C1.12 0 0 1.12 0 2.5C0 3.88 1.12 5 2.5 5H3.5C3.5 5 4 5 4 4.5C4 4 3.5 4 3.5 4H2.5C1.645 4 1 3.355 1 2.5ZM3 3H7C7 3 7.5 3 7.5 2.5C7.5 2 7 2 7 2H3C3 2 2.5 2 2.5 2.5C2.5 3 3 3 3 3ZM7.5 0H6.5C6.5 0 6 0 6 0.5C6 1 6.5 1 6.5 1H7.5C8.355 1 9 1.645 9 2.5C9 3.355 8.355 4 7.5 4H6.5C6.5 4 6 4 6 4.5C6 5 6.5 5 6.5 5H7.5C8.88 5 10 3.88 10 2.5C10 1.12 8.88 0 7.5 0Z"
+                fill="#AFD8D8"
+              />
+            </svg>
           </a>
         </div>
       </DescHeader>
@@ -97,11 +123,18 @@ const DescHeader = styled.div`
     div {
       color: ${(props) => props.theme.listItem.textAuthor};
       display: flex;
-      span {
+      span:first-child {
         margin-right: 4px;
       }
       svg {
         margin-right: 4px;
+      }
+    }
+    a {
+      display: flex;
+      align-items: center;
+      svg {
+        margin-left: 2px;
       }
     }
   }
